7941b1867845b526789a2eefaf003d9bfe98e727
Sasha Zhegulov <zhegulov.sasha@gmail.com>
2023-09-25 15:26:50 +0300

    dfdf

--- index.ts
+++ index.ts
@@ -1,75 +1,15 @@
-// структура данных склада с одеждой
-
-interface ClothesWarehouse {
-  jackets: "empty" | number;
-  hats: "empty" | number;
-  socks: "empty" | number;
-  pants: "empty" | number;
+interface IUser {
+  login: string;
+  password: number;
+  email: string;
+  avatar: string | undefined;
 }
 
-// структура данных склада с канцтоварами
-
-interface StationeryWarehouse {
-  scissors: "empty" | number;
-  paper: "empty" | boolean;
-}
-
-// структура данных склада с бытовой техникой
-
-interface AppliancesWarehouse {
-  dishwashers: "empty" | number;
-  cookers: "empty" | number;
-  mixers: "empty" | number;
-}
-
-// общая структура данных, наследует все данные из трех выше
-// + добавляет свои
-
-interface TotalWarehouse
-  extends ClothesWarehouse,
-    StationeryWarehouse,
-    AppliancesWarehouse {
-  deficit?: boolean;
-  date?: Date;
-}
-
-// главный объект со всеми данными, должен подходить под формат TotalWarehouse
-
-const totalData: TotalWarehouse = {
-  jackets: 5,
-  hats: "empty",
-  socks: "empty",
-  pants: 15,
-  scissors: 15,
-  paper: true,
-  dishwashers: 3,
-  cookers: "empty",
-  mixers: 14,
+const userData: IUser = {
+  login: "Thomas",
+  password: 12345,
+  email: "2323@fff.ru",
+  avatar: "",
 };
 
-// Реализуйте функцию, которая принимает в себя главный объект totalData нужного формата
-// и возвращает всегда строку
-// Функция должна отфильтровать данные из объекта и оставить только те названия товаров, у которых значение "empty"
-// и поместить их в эту строку. Если таких товаров нет - возвращается другая строка (см ниже)
-
-// С данным объектом totalData строка будет выглядеть:
-// "We need this items: hats, socks, cookers"
-// Товары через запятую, в конце её не должно быть. Пробел после двоеточия, в конце строки его нет.
-
-function printReport(data: TotalWarehouse): string {
-  const filteredData: string[] = [];
-
-  for (let key in data) {
-    if (data[key as keyof TotalWarehouse] === "empty") {
-      filteredData.push(key);
-    }
-  }
-
-  if (filteredData.length > 0) {
-    return `We need this items: ${filteredData.join(", ")}.`;
-  } else {
-    return "Everything fine";
-  }
-}
-
-console.log(printReport(totalData));
+console.log(userData);
